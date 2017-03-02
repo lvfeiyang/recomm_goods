5fa8f998fdeb9cb9831df88ec5fec8625ce1e58b
lvfeiyang <lxm19900207@163.com>
2017-03-02 17:55:59 +0800

    reduce samples

--- train.py
+++ train.py
@@ -10,5 +10,5 @@ def tran_data_generator():
             continue
 
 recomm_model = prepare_model.generate_recommend_model()
-recomm_model.fit_generator(tran_data_generator(), samples_per_epoch=2000, nb_epoch=50)
+recomm_model.fit_generator(tran_data_generator(), samples_per_epoch=200, nb_epoch=50)
 recomm_model.save_weights('recomm_weight.h5')
