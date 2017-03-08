f1c52abeb968d93c54421af38db9649e9f194445
lvfeiyang <lxm19900207@163.com>
2017-03-08 21:32:12 +0800

    up

--- train.py
+++ train.py
@@ -9,7 +9,8 @@ fileConfig('logging_config.ini')
 def tran_data_generator():
     for act_user in prepare_data.get_activity_users():
         try:
-            yield prepare_data.user_train_data(act_user).next()
+            for one_user_data in prepare_data.user_train_data(act_user):
+                yield one_user_data
         except Exception as e:
             logging.error("%s user have bad train data: %s" % (act_user, e))
             continue
