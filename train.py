b205f5cd495691eed82ac7d9d43453cb6d67c09d
lvfeiyang <lxm19900207@163.com>
2017-03-09 15:42:18 +0800

    up

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import gc
 import logging
 from logging.config import fileConfig
 from data import prepare_data, prepare_model
@@ -8,14 +7,14 @@ fileConfig('logging_config.ini')
 
 def tran_data_generator():
     for act_user in prepare_data.get_activity_users():
+        if 1 == act_user:
+            continue
         try:
             for one_user_data in prepare_data.user_train_data(act_user):
                 yield one_user_data
         except Exception as e:
             logging.exception("%s user have bad train data: %s" % (act_user, e))
             continue
-        finally:
-            gc.collect()
 
 recomm_model = prepare_model.generate_recommend_model()
 recomm_model.fit_generator(tran_data_generator(), samples_per_epoch=200, nb_epoch=50)
